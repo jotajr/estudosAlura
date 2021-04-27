c0c9cddd3c3f05461ea6a9bf10ad185a29178ae0
Jota Freitas Jr <jotafreitasjr@gmail.com>
2021-04-27 10:34:03 -0300

    Limpando o projeto react
    Limpando o projeto para deixar so o que nos precisaremos para o curso

--- react/entendendoComoFunciona/ceep/src/App.js
+++ react/entendendoComoFunciona/ceep/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite o arquivo <code>src/App.js</code> e salve para rendenrizar!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda React
-        </a>
-      </header>
-    </div>
+    <h1>Meu App</h1>
   );
 }
-
+// react -> lib
+// React --> Ecosistema
 export default App;
