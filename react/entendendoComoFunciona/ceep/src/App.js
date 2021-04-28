8bb58b631e0b6cada603049ad964b110caa3d456
Jota Freitas Jr <jotafreitasjr@gmail.com>
2021-04-28 11:41:56 -0300

    Usando state e props
    Foram utilizados nessas aulas como setar state e utilizar propriedades.

--- react/entendendoComoFunciona/ceep/src/App.js
+++ react/entendendoComoFunciona/ceep/src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
-import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas";
-import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
-
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import "./assets/index.css";
 class App extends Component {
-    render() {
-        return (
-          <section>
-            <FormularioCadastro />
-            <ListaDeNotas />
-          </section>
-        );
+  constructor() {
+    super();
+    this.state = {
+        notas:[]
+    };
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+        notas:novoArrayNotas
     }
+    this.setState(novoEstado);
+  }
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    );
+  }
 }
 
 export default App;
--- react/entendendoComoFunciona/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ react/entendendoComoFunciona/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-import CardNota from "../CardNota/CardNota";
+import CardNota from "../CardNota";
 import "./estilo.css";
-
 class ListaDeNotas extends Component {
+
   render() {
     return (
       <ul className="lista-notas">
-        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
+        {this.props.notas.map((nota, index) => {
           return (
-            <li className="lista-notas_item" key={index}>
-              <div>{categoria}</div>
-              <CardNota />
+            <li className="lista-notas_item" nota={nota} key={index}>
+              <CardNota titulo={nota.titulo} texto={nota.texto}/>
             </li>
           );
         })}
