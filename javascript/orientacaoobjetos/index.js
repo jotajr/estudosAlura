6e06f2ea63ae9768c24991f3b1177597744c67bc
Jota Freitas Jr <jotafreitasjr@gmail.com>
2021-05-03 09:14:34 -0300

    Adicionado modulo 3 de OO com JavaScript

---
+++ javascript/orientacaoobjetos/index.js
@@ -0,0 +1,24 @@
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
+
+const contaCorrenteAlice = new ContaCorrente();
+contaCorrenteAlice.agencia = 1002;
+contaCorrenteAlice.cliente = cliente2;
+
+contaCorrenteRicardo.transferir(200, contaCorrenteAlice);
+
+console.log(contaCorrenteRicardo);
+console.log(contaCorrenteAlice);
