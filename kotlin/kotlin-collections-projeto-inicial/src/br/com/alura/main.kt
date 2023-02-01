6219997f0f9eb888adbfc8bfd22d755e6333658a
Jota Freitas Jr <jotafreitasjr@gmail.com>
2023-02-01 14:55:36 -0300

    Adicionando o companion object

--- kotlin/kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
+++ kotlin/kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -2,10 +2,33 @@ package br.com.alura
 
 fun main() {
 
+    val banco = BancoDeNomes()
+    banco.salva("Jota Freitas Jr")
+    println(banco.nomes)
+    println(BancoDeNomes().nomes)
+
+}
+
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = Companion.dados
+
+    fun salva(nome: String) {
+        Companion.dados.add(nome)
+    }
+
+    // Com o companion object esta propriedade vira unica para todos as instancias deste objeto
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+private fun testaColecao() {
     println("Projeto inicial de collections com Kotlin")
 
-    val nomes : List<String> = listOf<String>("Jota", "Patricia", "Enzo", "Lais")
+    val nomes: MutableList<String> = mutableListOf("Jota", "Patricia", "Enzo", "Lais")
 
-    println(nomes)
+    nomes.add("Evellyn")
 
+    println(nomes.sorted())
 }
