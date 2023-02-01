4b4c571670e489e9dadad4440ba64cb5e7c4c572
Jota Freitas Jr <jotafreitasjr@gmail.com>
2023-02-01 14:42:50 -0300

    Iniciado o curso de collections kotlin

---
+++ kotlin/kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -0,0 +1,11 @@
+package br.com.alura
+
+fun main() {
+
+    println("Projeto inicial de collections com Kotlin")
+
+    val nomes : List<String> = listOf<String>("Jota", "Patricia", "Enzo", "Lais")
+
+    println(nomes)
+
+}
