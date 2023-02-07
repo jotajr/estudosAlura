c066accd6fb3de8875482d5ac99c892f35368f95
Jota Freitas Jr <jotafreitasjr@gmail.com>
2023-02-07 17:00:58 -0300

    Finalizado busca e filtros

--- react/aluroni/src/pages/Cardapio/index.tsx
+++ react/aluroni/src/pages/Cardapio/index.tsx
@@ -2,9 +2,11 @@ import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "./Buscador";
 import { useState } from "react";
+import Filtros from "./Filtros";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
   return (
     <main>
       <nav className={styles.menu}>
@@ -16,6 +18,9 @@ export default function Cardapio() {
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+            <Filtros filtro={filtro} setFiltro={setFiltro} />
+        </div>
       </section>
     </main>
   );
