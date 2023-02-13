49bd7bd5837ef1188d232e08e65423a47e3c5769
Jota Freitas Jr <jotafreitasjr@gmail.com>
2023-02-13 17:22:45 -0300

    Adicionando as rotas iniciais

--- react/aluroni/src/index.tsx
+++ react/aluroni/src/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
+import AppRouter from 'routes';
+
+/* const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio />; */
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Cardapio />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        {/* <Cardapio /> */}
+        {/* <Inicio /> */}
+        {/* {componenteAtual} */}
+        <AppRouter />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
--- react/aluroni/src/pages/Cardapio/Filtros/index.tsx
+++ react/aluroni/src/pages/Cardapio/Filtros/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import filtros from './filtros.json';
+import filtros from 'data/filtros.json';
 import styles from './Filtro.module.scss';
 import classNames from 'classnames';
 
@@ -11,24 +11,24 @@ interface Props {
 }
 
 export default function Filtros({ filtro, setFiltro }: Props) {
-  function selecionarFiltro(opcao: IOpcao) {
-    if (filtro === opcao.id) return setFiltro(null);
-    return setFiltro(opcao.id);
-  }
-  return (
-    <div className={styles.filtros}>
-      {filtros.map((opcao) => (
-        <button
-          className={classNames({
-            [styles.filtros__filtro]: true,
-            [styles['filtros__filtro--ativo']]: filtro === opcao.id,
-          })}
-          key={opcao.id}
-          onClick={() => selecionarFiltro(opcao)}
-        >
-          {opcao.label}
-        </button>
-      ))}
-    </div>
-  );
+    function selecionarFiltro(opcao: IOpcao) {
+        if (filtro === opcao.id) return setFiltro(null);
+        return setFiltro(opcao.id);
+    }
+    return (
+        <div className={styles.filtros}>
+            {filtros.map((opcao) => (
+                <button
+                    className={classNames({
+                        [styles.filtros__filtro]: true,
+                        [styles['filtros__filtro--ativo']]: filtro === opcao.id,
+                    })}
+                    key={opcao.id}
+                    onClick={() => selecionarFiltro(opcao)}
+                >
+                    {opcao.label}
+                </button>
+            ))}
+        </div>
+    );
 }
