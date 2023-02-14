0fb95c626185a95c89d9769ce3475853fab73c73
Jota Freitas Jr <jotafreitasjr@gmail.com>
2023-02-14 10:00:58 -0300

    Finalizando sobre e notfound

--- react/aluroni/src/pages/Cardapio/Itens/Item/index.tsx
+++ react/aluroni/src/pages/Cardapio/Itens/Item/index.tsx
@@ -5,34 +5,34 @@ import classNames from 'classnames';
 type Props = typeof cardapio[0];
 
 export default function Item(props: Props) {
-  const {title, description, category, size, serving, price, photo} = props;
-  return (
-    <div className={styles.item}>
-      <div className={styles.item__imagem}>
-        <img src={photo} alt={title} />
-      </div>
-      <div className={styles.item__descricao}>
-        <div className={styles.item__titulo}>
-          <h2>{title}</h2>
-          <p>{description}</p>
+    const { title, description, category, size, serving, price, photo } = props;
+    return (
+        <div className={styles.item}>
+            <div className={styles.item__imagem}>
+                <img src={photo} alt={title} />
+            </div>
+            <div className={styles.item__descricao}>
+                <div className={styles.item__titulo}>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                </div>
+                <div className={styles.item__tags}>
+                    <div
+                        className={classNames({
+                            [styles.item__tipo]: true,
+                            [styles[`item__tipo__${category.label.toLowerCase()}`]]: true,
+                        })}
+                    >
+                        {category.label}
+                    </div>
+                    <div className={styles.item__porcao}>{size} g</div>
+                    <div className={styles.item__qtdpessoas}>
+            Serve {serving} Pessoa
+                        {serving === 1 ? '' : 's'}
+                    </div>
+                    <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
+                </div>
+            </div>
         </div>
-        <div className={styles.item__tags}>
-          <div
-            className={classNames({
-              [styles.item__tipo]: true,
-              [styles[`item__tipo__${category.label.toLowerCase()}`]]: true,
-            })}
-          >
-            {category.label}
-          </div>
-          <div className={styles.item__porcao}>{size} g</div>
-          <div className={styles.item__qtdpessoas}>
-              Serve{serving === 1 ? '' : 'm'} {serving} Pessoa
-            {serving === 1 ? '' : 's'}
-          </div>
-          <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
--- react/aluroni/src/routes.js
+++ react/aluroni/src/routes.js
@@ -1,22 +1,26 @@
+import Footer from 'components/Footer';
 import Header from 'components/Header';
 import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import NotFound from 'pages/NotFound';
 import Sobre from 'pages/Sobre';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 export default function AppRouter() {
     return (
-        <main>
+        <main className='container'>
             <Router>
                 <Menu />
                 <Routes>
                     <Route path="/" element={<Header />}>
                         <Route index element={<Inicio />} />
-                        <Route path='cardapio' element={<Cardapio />} />
-                        <Route path='sobre' element={<Sobre />} />
+                        <Route path="cardapio" element={<Cardapio />} />
+                        <Route path="sobre" element={<Sobre />} />
                     </Route>
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
+                <Footer />
             </Router>
         </main>
     );
